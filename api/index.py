1c291bd8f4fe75b2d14e32f5a7e4c94fd6402c80
aixiu <4815563@qq.com>
2022-10-24 01:34:33 +0800

    init

--- api/index.py
+++ api/index.py
@@ -1,12 +1,19 @@
-from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+# coding:utf-8
+import uvicorn
+from fastapi import FastAPI, Response
+from api.crawler import main as new
 
-class handler(BaseHTTPRequestHandler):
+app = FastAPI()
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
-        return
+@app.get("/api")
+def news(response: Response, index: int = 0, origin: str = 'zhihu', cache: str = 'null'):
+    response.headers["Cache-Control"] = "max-age=86400, immutable, stale-while-revalidate"
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    if origin == "undefined":
+        origin = "zhihu"
+    return new(index, origin)
+
+
+if __name__ == "__main__":
+    uvicorn.run("index:app", host="127.0.0.1", port=62, log_level="info")
