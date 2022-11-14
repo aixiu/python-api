752729b523a19bbdd845fff93d001ac0163103bb
aixiu <4815563@qq.com>
2022-11-14 15:51:19 +0800

    输出格式

--- bing_api.py
+++ bing_api.py
@@ -16,8 +16,7 @@ class BingWallpaper():
     def __init__(self) -> None:
         self.url = 'https://cn.bing.com/'
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'
-        self.headers = {'User-Agent': self.user_agent}
-        
+        self.headers = {'User-Agent': self.user_agent}        
         
     def get_json_data(self, fa='js', idx=0, n=1, mkt='zh-CN'):
         try:
